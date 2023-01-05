39a23e366f8cb097b1aa13db6f3202c25489189a
crasride <jose.dfernandez@yahoo.fr>
2023-01-05 17:29:58 +0100

    update

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+apt-get update && apt-get install -y make golang-go git curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o h.deb
 apt install ./h.deb
