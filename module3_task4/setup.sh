f5abc67bae401265d96ee95aef6c4a44e6157f14
crasride <105241291+crasride@users.noreply.github.com>
2023-01-08 17:34:15 +0100

    Update setup.sh

--- module3_task4/setup.sh
+++ module3_task4/setup.sh
@@ -2,6 +2,6 @@
 apt-get update && apt-get install -y hugo make
 apt-get install curl -y
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-apt-get install ./install_hugo.deb
+sudo apt-get install ./install_hugo.deb
 npm install -g markdownlint-cli
 rm ./install_hugo.deb
