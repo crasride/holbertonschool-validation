2de84001717e503cacf5564143822aa19916259b
crasride <105241291+crasride@users.noreply.github.com>
2023-01-12 12:09:34 +0100

    Update setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
 sudo apt-get install ./install_hugo.deb
-sudo apt-get remove golang-go
-sudo apt-get remove --auto-remove golang-go
