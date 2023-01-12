53000f8f51c6baeb97f10f00b3932ca9c607680b
crasride <jose.dfernandez@yahoo.fr>
2023-01-12 12:29:58 +0100

    update

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-sudo apt-get install ./install_hugo.deb
+apt-get install ./install_hugo.deb
