eb681706b87878cabfd1fd45a4c3c0e074bc4f6e
crasride <105241291+crasride@users.noreply.github.com>
2023-01-16 10:41:33 +0100

    Delete module4_task1 directory

--- module4_task1/content/posts/welcome.md
+++
@@ -1,6 +0,0 @@
----
-title: "Welcome"
-date: 2023-01-03T16:26:24+01:00
-draft: false
----
-
--- module4_task1/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-sudo apt install ./hugo.deb
-npm install -g markdownlint-cli
-rm hugo.deb
