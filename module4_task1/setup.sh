fd59bf71af90250d4d2ea95be43a28df10b4e48f
crasride <105241291+crasride@users.noreply.github.com>
2023-01-16 10:30:16 +0100

    Update setup.sh

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-rm -rf /usr/local/go
-sudo apt update
-sudo apt install zip -y
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo apt install ./hugo.deb
+npm install -g markdownlint-cli
+rm hugo.deb
