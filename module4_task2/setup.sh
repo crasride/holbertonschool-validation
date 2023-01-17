e7744b960acd465a7eca9065fdbbf95696eb7244
crasride <105241291+crasride@users.noreply.github.com>
2023-01-17 14:41:34 +0100

    Update setup.sh

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,12 +1,8 @@
+
 #!/usr/bin/env bash
-apt-get update && apt-get install -y hugo make
-apt-get install curl -y
-curl -Lo hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-sudo apt-get install ./hugo.deb
-npm install -g markdownlint-cli
-rm ./install_hugo.deb
-sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64
-sudo chmod +x /bin/hadolint
-curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64
-chmod +x container-structure-test-linux-amd64
-sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
+apt-get install make wget
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo apt-get install ./install_hugo.deb
+rm install_hugo.deb
+wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 && sudo chmod +x /bin/hadolint
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
