9228c396ba458025e018e521211d2b6a0f84e2ad
crasride <jose.dfernandez@yahoo.fr>
2023-01-17 13:46:14 +0100

    update setup for task

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-rm -rf /usr/local/go
-sudo apt update
-sudo apt install zip -y
+apt-get update && apt-get install -y hugo make
+apt-get install curl -y
+curl -Lo hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+apt-get install ./hugo.deb
+npm install -g markdownlint-cli
+rm ./install_hugo.deb
+sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64
+sudo chmod +x /bin/hadolint
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64
+chmod +x container-structure-test-linux-amd64
+sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
