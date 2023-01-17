a7504a9ce6c52e8fd33de6ca733e5b239cd459c7
crasride <jose.dfernandez@yahoo.fr>
2023-01-17 14:27:28 +0100

    update setup.sh

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 apt-get update && apt-get install -y hugo make
 apt-get install curl -y
 curl -Lo hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-apt-get install ./hugo.deb
+sudo apt-get install ./hugo.deb
 npm install -g markdownlint-cli
 rm ./install_hugo.deb
 sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-x86_64
