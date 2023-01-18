7a5d86c8222cdfe6fdda1b29c9d1be68f13cde8d
crasride <jose.dfernandez@yahoo.fr>
2023-01-18 16:32:07 +0100

    update

---
+++ module4_task2/setup.sh
@@ -0,0 +1,8 @@
+
+#!/usr/bin/env bash
+apt-get install make wget
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+sudo apt-get install ./install_hugo.deb
+rm install_hugo.deb
+wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64 && sudo chmod +x /bin/hadolint
+curl -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 && chmod +x container-structure-test-linux-amd64 && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
